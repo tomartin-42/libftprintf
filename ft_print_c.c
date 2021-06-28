6d17ec15757b447a2a341df08c04fa29ace84c97
tomartin-42 <tomasmartinezcurz@gmail.com>
2021-06-28 12:58:53 +0200

    tommy 28/06/2021 12:58

--- ft_print_c.c
+++ ft_print_c.c
@@ -5,20 +5,35 @@ void	ft_print_c (t_flags *flags, va_list *pf_arg)
 	char	c;
 	char	*to_print;
 
+	imprimeflags (flags);	
 	c = va_arg (*pf_arg, char);
 	if (flags->width > 0)
-		to_print = malloc (flags->width);
-	else
-		to_print = malloc (1);
-	if (flags->zero == true && flags->alig == false)
-		ft_make_string_zero (to_print, flags->width);
-	if (flags->alig == false)
 	{
-		ft_make_string_space (to_print, flags->width);
-		to_print[0] = c;
+		*to_print =  malloc (flags->width);
 	}
 	else
+	{
+		*to_print = malloc (1);
+	}
+	to_print[flags->width] = '\0';
+	ft_make_string_space (to_print);
+	printf ("[[%s]]\n", to_print);
+	if (flags->alig == true)
+	{
+		ft_make_string_space (to_print);
+	}
+	if (flags->zero == true && flags->alig == false)			
+	{
+		ft_make_string_zero (to_print);
+	}
+	if (flags->alig == false)
+	{
+		to_print[0] = c;
+	}
+	if (flags->alig == true)
+	{
 		to_print[flags->width] = c;
+	}
 	ft_to_write (to_print, flags->width, flags);
-	free (to_print);
+	//free (to_print);
 }
--- ft_writes.c
+++ ft_writes.c
@@ -1,28 +1,29 @@
 #include "ft_printf.h"
 
-void	ft_make_string_zero (char *string, int leng)
+void	ft_make_string_zero (char *string)
 {
-	while (leng > 0)
-	{
-		string[leng] = '0';
-		leng--;
-	}
+	int	i;
+
+	i = 0;
+	while (string[i] != '\0')
+		string[i++] = '0';
 }
 
-void	ft_make_string_space (char *string, int leng)
+void	ft_make_string_space (char *string)
 {
-	while (leng > 0)
-	{
-		string[leng] = ' ';
-		leng--;
-	}
+	int	i;
+
+	i = 0;
+	while (string[i] != '\0')
+		string[i++] = ' ';
 }
 
 int		ft_to_write (char *to_print, int leng, t_flags *flags)
 {
 	int	i;
 	i = 0;
-	while (i < leng)
+
+	while (i <= leng)
 	{
 		write (1, &to_print[i], 1);
 		i++;
