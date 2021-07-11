0b51efb7b9c860b86cc0eb568ec98ce2b29c70ce
Tommy <tommy@localhost.localdomain>
2021-07-11 08:50:02 +0200

    tommy 11/07/2021 19:34 DOMINGO

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 # include "ft_printf.h"
 
 int main (void)
-{
+{/*
 	// -------------- %c --------------- 
 	char	 cr = 'o';
  	//printf ("AAA%-10cAAAAAA\n", cr);
@@ -75,5 +75,34 @@ printf ("123456789012345678901234567890\n");
 	
 	//printf ("123456789012345678901234567890\n");
 	printf ("[[%d - %d]]\n", x, y);
- return (1);
+ return (1);*/
+
+ 	int		a = -4;
+	int		b = 0;
+	char	c = 'a';
+	int		d = 2147483647;
+	int		e = -2147483648;
+	int		f = 42;
+	int		g = 25;
+	int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
+	char	*n = "abcdefghijklmnop";
+	char	*o = "-a";
+	char	*p = "-12";
+	char	*q = "0";
+	char	*r = "%%";
+	char	*s = "-2147483648";
+	char	*t = "0x12345678";
+	char	*u = "-0";
+
+	printf("Simple input test\n");
+	ft_printf("Simple input test\n");
+	printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j); //T2
+	ft_printf("%i, %d, %d, %d, %d, %s, %c, %d, %u, %x, %X\n", i, j, k, l, m, n, c, c, j, j, j); //T2
+	printf("%0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d); //T3
+	ft_printf("%0i, %0d, %0d, %0d, %0d, %0d, %0d, %0d\n", i, j, k, l, m, c, e, d); //T3
 }
