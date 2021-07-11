1070e950489f3445cc6cecc67091dc694e0f7a9b
tomartin-42 <tomasmartinezcurz@gmail.com>
2021-07-11 20:07:04 +0200

    tommy 11/07/2021 20:07 NORMA

--- ft_print_por.c
+++ ft_print_por.c
@@ -1,15 +1,16 @@
 #include "ft_printf.h"
 
-static void	ft_print_por_unalig (t_flags *flg, char c, char *num)
+static void	ft_print_por_unalig (t_flags *flg, char c, char num)
 {	
 	ft_make_string_zero (flg->width - 1, flg, c);
-	ft_to_write (num, 1, flg);
+	ft_to_write (&num, 1, flg);
 }
 
-static void	ft_print_por_alig (t_flags *flg, char *num)
+static void	ft_print_por_alig (t_flags *flg, char c, char num)
 {
-	ft_to_write (num, 1, flg);
-	ft_make_string_zero (flg->width - 1, flg, ' ');
+	c = ' ';
+	ft_to_write (&num, 1, flg);
+	ft_make_string_zero (flg->width - 1, flg, c);
 }
 
 void	ft_print_por (t_flags *flg)
@@ -22,7 +23,7 @@ void	ft_print_por (t_flags *flg)
 	if (flg->zero == true)
 		c = '0';
 	if (flg->alig == false)
-		ft_print_por_unalig (flg, c, &num);
+		ft_print_por_unalig (flg, c, num);
 	else if (flg->alig == true)
-		ft_print_por_alig (flg, c, &num);
+		ft_print_por_alig (flg, c, num);
 }
